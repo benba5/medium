b23fcc9064e9cf424a46a886314fdbfec86564ac
Ben <ben@appsynth.net>
2017-04-13 09:55:37 +0700

    Add Example1

---
+++ DependencyInjection/Example1.swift
@@ -0,0 +1,15 @@
+//
+//  Example1.swift
+//  
+//
+//  Created by appsynth on 4/13/17.
+//
+//
+
+import Foundation
+
+class StoreManager { }
+
+class ViewController: UIViewController {
+    var storeManager: StoreManager?
+}
